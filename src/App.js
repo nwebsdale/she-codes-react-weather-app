dab2fc31ae3dfe40e0be9fd5697e601fea509a98
nwebsdale <natashawebsdale@hotmail.co.uk>
2021-03-14 14:03:51 +0000

    Added search function (updates H1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import Search from "./Search.js";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <button className="btn btn-danger">Hello?</button>
+        <Search />
+        <div className="footer">
+          <a
+            className="open-source"
+            href="https://github.com/nwebsdale/she-codes-react-weather-app"
+            target="_blank"
+            target="_blank"
+          >
+            Open-source code
+          </a>
+          <p> by Natasha Websdale</p>
+        </div>
       </div>
     </div>
   );
