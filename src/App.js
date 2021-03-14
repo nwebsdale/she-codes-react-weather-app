e71015da67fbee8914674ef5459a17a567ba8c31
nwebsdale <natashawebsdale@hotmail.co.uk>
2021-03-14 12:45:15 +0000

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">hello?</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <button className="btn btn-danger">Hello?</button>
+      </div>
+    </div>
+  );
 }
